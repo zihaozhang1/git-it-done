d86356647e749d7c40a2c11ed46d95b5ba15a938
Zihao <zihaozhang1@gmail.com>
2020-11-19 07:55:03 -0500

    added error handling

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -6,10 +6,20 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      displayRepos(data, user);
-    });
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
@@ -34,6 +44,11 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 var nameInputEl = document.querySelector("#username");
 
 var displayRepos = function(repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   console.log(repos);
   console.log(searchTerm);
   // clear old content
